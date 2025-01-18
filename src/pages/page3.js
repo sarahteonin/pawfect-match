bdc78daf3259db7f6712b33fbe590514e9d023ac
jasmiinee <jasmiine.lje@gmail.com>
2025-01-18 13:34:42 +0800

    Add pages

---
+++ src/pages/page3.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Page3() {
+    return (
+        <div>
+            <h1>Midday activities</h1>
+        </div>
+    );
+}
---
+++ src/pages/page5.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Page5() {
+    return (
+        <div>
+            <h1>An Unexpected adventure</h1>
+        </div>
+    );
+}
