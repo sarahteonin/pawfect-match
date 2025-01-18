3f8c49021b596d6d04c21095a79781cf71cde310
jasmiinee <jasmiine.lje@gmail.com>
2025-01-18 14:49:25 +0800

    page 6

--- src/pages/page6.js
+++ src/pages/page6.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import "../index.css";
 
 export default function Page6() {
+
+    const handleNext = () => {
+        window.location.href = "/7";
+    };
+
     return (
         <div>
-            <h1>The forest path</h1>
+            <h2>
+                You find yourself in a dense forest with new sights and smells.
+                <br/>
+                What do you focus on?
+            </h2>
+
+            <button onClick={handleNext}>Follow the sound of running water—it seems peaceful.</button>
+            <button onClick={handleNext}>Chase a squirrel you just spotted.</button>
+            <button onClick={handleNext}>Carefully sniff every tree and track the scent trail.</button>
+            <button onClick={handleNext}>Look for signs of other animals or humans nearby.</button>
         </div>
+        
     );
 }
