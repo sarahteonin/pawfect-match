736b3dd0169cab834714c6dc9bb6d6e865014479
sarahteonin <sarahfaithteo@gmail.com>
2025-01-18 14:25:04 +0800

    homepage outline

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import "../index.css";
 
 export default function Homepage() {
+    const handleStart = () => {
+        window.location.href = "/1";
+    };
+
     return (
         <div>
-            <h1>Homepage</h1>
+            <h1>What dog breed are you?</h1>
+            <h2>
+                Imagine you’ve woken up as a dog for a day! 🐾 
+                <br/>
+                Let’s explore your doggy instincts to uncover which breed matches your personality.
+            </h2>
+            <button onClick={handleStart}>Start</button>
         </div>
     );
 }
