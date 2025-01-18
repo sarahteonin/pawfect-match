a2f79dd45f7875bda64437fa1e2fd4caf413d8aa
sarahteonin <sarahfaithteo@gmail.com>
2025-01-18 14:44:12 +0800

    page 1 done

--- src/pages/page1.js
+++ src/pages/page1.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
 export default function Page1() {
+    const handleNext = () => {
+        window.location.href = "/2";
+    };
+
     return (
         <div>
-            <h1>A New Day Begins</h1>
+            <h2>You wake up to the sound of birds chirping. 
+                <br/>
+                What’s your first move?
+            </h2>
+            <div className={"button-container"}>
+                <button onClick={handleNext}>Stretch out and roll around lazily in your cozy bed.</button>
+                <button onClick={handleNext}>Dash outside to explore the backyard.</button>
+                <button onClick={handleNext}>Run straight to the food bowl—breakfast time!</button>
+                <button onClick={handleNext}>Sit and wait for your human to wake up, ready to assist them.</button>
+            </div>
         </div>
     );
 }
